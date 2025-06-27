d4fdf64f6d09d1875383f0771d961bb7b950efd0
Pz0324 <00062019@uca.edu.sv>
2025-06-27 11:35:16 -0600

    changing url cors

--- HappyPaws-backend/src/main/java/org/ncapas/happypawsbackend/config/AuthConfig.java
+++ HappyPaws-backend/src/main/java/org/ncapas/happypawsbackend/config/AuthConfig.java
@@ -57,7 +57,7 @@ public class AuthConfig  {
                 .cors(cors -> cors
                         .configurationSource(request -> {
                             CorsConfiguration config = new CorsConfiguration();
-                            config.setAllowedOrigins(List.of("http://localhost:5173"));
+                            config.setAllowedOrigins(List.of("https://happypaws-fe.vercel.app/"));
                             config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
                             config.setAllowedHeaders(List.of("*"));
                             config.setAllowCredentials(true);
@@ -130,7 +130,7 @@ public class AuthConfig  {
     @Bean
     public org.springframework.web.cors.CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("https://happypaws-fe.vercel.app/"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
